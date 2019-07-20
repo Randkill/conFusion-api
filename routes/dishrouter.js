68f4894e538fbe68e6c428de11b3775e3166aa58
Randkill <randkill.randkill@gmail.com>
2019-07-20 17:38:10 +0430

    connecting dishriuter.js to the mongoDB

--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -2,27 +2,56 @@
 //Also this module can be imported and be MOUNTED in index.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes = require('../models/dishes');
 
 const dishRouter = express.Router();    //this will declare dishrouter.js as an Expres Router
 dishRouter.use(bodyParser.json());
 
 //it supports below method:
 //it means that we can chaining all the GET,POST,PUT,DELETE methods easier to this dishrouter
-dishRouter.route('/').all((req,res,next) => {      //app.all() : no matter which method of GET,POST,PUT etc is called
+dishRouter.route('/')/*.all((req,res,next) => {      //app.all() : no matter which method of GET,POST,PUT etc is called
     res.statusCode = 200;                   //this app.all() will be executed first!
     res.setHeader('Content-Type', 'text/html');
     next();        //it means it will continue on to look for additional specifications 
                    //which match /dishes end point (means it will continue till the next word)
-}).get((req,res,next) => {
-    res.end('Will send all the dishes to you!');
+})*/.get((req,res,next) => {
+    //res.end('Will send all the dishes to you!');
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }).catch((err) => {
+        next(err);      //passing the ERROR to the overall error-handler
+    })
 }).post((req,res,next) => {
-    res.end('Will add the dish : ' + req.body.name + 
-    ' with details : ' + req.body.description);
+    //res.end('Will add the dish : ' + req.body.name + 
+    //'with details : ' + req.body.description);
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('--> dish created :\n', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }).catch((err) => {
+        next(err);      //passing the ERROR to the overall error-handler
+    })
 }).put((req,res,next) => {
     res.statusCode = 403;       //not supported code
     res.end('PUT operation not supported on /dishes !');
 }).delete((req,res,next) => {
-    res.end('Deleting all the dishes!');
+    //res.end('Deleting all the dishes!');
+    //removing all the dishes from the server!!
+    Dishes.remove({})
+    .then((response) => {
+        es.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    }).catch((err) => {
+        next(err);      //passing the ERROR to the overall error-handler
+    })
 });
 
 //now handling /dishes/:dishId
